7be871a2b8479e535921da33784ed160fb2d3ae6
dong <dong@localhost.localdomain>
2020-11-20 05:46:18 -0500

    init

---
+++ install-docker-fedora32.sh
@@ -0,0 +1,21 @@
+#remove conflicts
+dnf remove docker-* -y ; dnf config-manager --disable docker-* -y ;
+#syste prep
+grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0" &&
+#enable old cgroups
+grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0" &&
+# whitelist docker in firewall 
+firewall-cmd --permanent --zone=trusted --add-interface=docker0 &&
+firewall-cmd --permanent --zone=FedoraWorkstation --add-masquerade &&
+# installing moby
+dnf install moby-engine docker-compose &&
+# systemctl enable
+systemctl enable docker &&
+systemctl start docker &&
+
+# running as admin
+groupadd docker
+usermod -aG docker dong
+
+
+
